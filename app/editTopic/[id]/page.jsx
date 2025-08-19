77908a4b1382e014c5b890ebac4970bc2a7a1e60
Kartik Turak <kartikturak1@gmail.com>
2025-08-20 01:17:43 +0530

    UI Done

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const editTopic = () => {
+  return (
+    <form className='flex flex-col gap-3'>
+        <input className='border border-slate-500 px-8 py-2 ' type="text" placeholder='Topic Title'/>
+        <input className='border border-slate-500 px-8 py-2 ' type="text" placeholder='Topic Description'/>
+
+        <button className='bg-green-600 font-bold  text-white py-3 px-6 w-fit'>
+            Edit Topic
+        </button>
+    </form>
+  )
+}
+
+export default editTopic
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,13 @@
+import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
   return (
-    <h1>Notes app</h1>
+    <>
+      <TopicsList/>
+      <TopicsList/>
+      <TopicsList/>
+      <TopicsList/>
+      <TopicsList/>
+    </>
   );
 }
